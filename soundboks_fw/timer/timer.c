6bbccba0005a2775f7dbde827f48c9a9b35a1456
viktorio <vicktorio@gmail.com>
2021-11-10 18:46:52 +0100

    changes: - refactored the lib abit - added documentation

--- soundboks_fw/timer/timer.c
+++ soundboks_fw/timer/timer.c
@@ -1,10 +1,13 @@
 #include "timer.h"
 #include <limits.h>
+#include <stdbool.h>
 
+// ** Table of `MAX_N_SOFT_TIMERS` soft_timers
 static Timer_t soft_timers[MAX_N_SOFT_TIMERS] =
 {
-    {0UL, 0UL, false, false, NULL, NULL}
+    {0UL, 0UL, {0}, NULL, NULL}
 };
+static size_t last_i = 0;
 
 inline Timer_t * timer_start(uint32_t period, 
              bool periodic, 
@@ -12,11 +15,11 @@ inline Timer_t * timer_start(uint32_t period,
              void * arg) {
     //Find first available timer
     size_t i;
-    for(i = 0;i < MAX_N_SOFT_TIMERS; ++i) {
-        if(!soft_timers[i].running) {
+    for(i = last_i;i < MAX_N_SOFT_TIMERS; ++i) {
+        if(!soft_timers[i].flags.running) {
             soft_timers[i].period = period;
-            soft_timers[i].running = true;
-            soft_timers[i].periodic = periodic;
+            soft_timers[i].flags.running = true;
+            soft_timers[i].flags.periodic = periodic;
             soft_timers[i].clbk = clbk;
             soft_timers[i].arg = arg;
             soft_timers[i].stime = get_clock();
@@ -26,37 +29,36 @@ inline Timer_t * timer_start(uint32_t period,
     return NULL; //No available timers
 }
 
+
 inline void timer_stop(Timer_t * tmr) {
-    tmr->running = false;
+    tmr->flags.running = 0;
 }
 
 inline bool is_running(Timer_t * tmr) {
-    return tmr->running;
+    return tmr->flags.running;
 }
 
 void update_timers() {
-    uint32_t end;
+    uint32_t ctime;
     for(size_t i = 0;i < MAX_N_SOFT_TIMERS; ++i) {
-        if(soft_timers[i].running) {
-            end = get_clock();
+        if(soft_timers[i].flags.running) {
+            ctime = get_clock();
             uint32_t delta;
-            if(soft_timers[i].stime > end) {
-                delta = ULONG_MAX - end + soft_timers[i].stime;
+            if(soft_timers[i].stime > ctime) {
+                delta = ULONG_MAX - ctime + soft_timers[i].stime;
             }
             else {
-                delta = end - soft_timers[i].stime;
+                delta = ctime - soft_timers[i].stime;
             }
             
             if(delta > soft_timers[i].period) {
-                soft_timers[i].running = soft_timers[i].periodic;
-                soft_timers[i].stime = end;
+                soft_timers[i].flags.running = soft_timers[i].flags.periodic;
+                if(!soft_timers[i].flags.running) {
+                    last_i = i;
+                }
+                soft_timers[i].stime = ctime;
                 soft_timers[i].clbk(soft_timers[i].arg);
             }
         }
     }
-}
-
-int callback_register(timer_callback_t cb, size_t time, void *ctx) {
-    //@TODO: Implement
-    return 0;
 }
--- soundboks_fw/timer/timer.h
+++ soundboks_fw/timer/timer.h
@@ -3,7 +3,6 @@
 
 #include <stddef.h>
 #include <stdint.h>
-#include <stdbool.h>
 
 #ifndef MAX_N_SOFT_TIMERS
 #define MAX_N_SOFT_TIMERS 5
@@ -16,21 +15,59 @@ typedef int (*timer_callback_t)(void *ctx);
 typedef struct {
     uint32_t period,
             stime;
-    bool running,
-         periodic;
+    struct {
+        uint32_t running : 1;
+        uint32_t periodic : 1;
+    } flags;
     timer_callback_t clbk;
     void * arg;
 } Timer_t;
 
-extern void timer_stop(Timer_t * tmr);
-extern bool is_running(Timer_t * tmr);
+
+/**  @brief A function that starts a soft-timer from a table of timers. 
+*
+* \details The function first looks for a non-running timer in a table of software timers, and starts this timer.
+* This timer can be one time, i.e. it expires only once, or periodic, in which case it expires indefinitely,
+* until stopped manually. A callback function is called upon expiration.
+
+* @param period The period, or expire time, of the timer
+* @param periodic Setting this to false will start a one-time timer; `true` starts a periodic timer.
+* @param clbk A pointer to a callback function
+* @param arg A pointer to an argument that is passed to the callback function
+* @return Returns a pointer to an available timer after it starts; returns NULL if no timer is available.
+*/
 extern Timer_t * timer_start(uint32_t period, 
              bool periodic, 
              timer_callback_t clbk,
              void * arg);
+/**
+ * @brief Stops a timer.
+ * 
+ * @param tmr The pointer handle to a timer to be stopped.
+ */
+extern void timer_stop(Timer_t * tmr);  
+/**
+ * @brief Tests if a timer is running.
+ * 
+ * @param tmr The pointer handle to a timer to be stopped.
+ * @return `true` whenever the timer is running, false otherwise.
+ */
+extern bool is_running(Timer_t * tmr);   
+/**
+ * @brief Function to update the timers, based on the global timer reference.
+ * @details This function takes care that the timers are compared with a reference clock,
+ *   and a callback is called if the time delta is greater than the setup period. If a
+ *   timer is not periodic, then this function also stops the timer, by setting its
+ *   running flag to false. This function should be periodically called by an external process.
+ *   This means that the callbacks are executed within the thread of this process.
+ */        
 extern void update_timers();
+/**
+ * @brief Gets the current time in clock cycles.
+ * @details This function is a stub, and must be implemented by the user. The function
+ *   must return the elapsed time in clock cycles from system start-up. 
+ * @return The clock cycle count 
+ */
 extern uint32_t get_clock();
-// extern void print_timers();
-// int callback_register(timer_callback_t cb, size_t time, void *ctx);
 
 #endif // TIMER_H
